9d8c827a947da755093ed742e9747e2f4bb29bf3
rafael.arias.gomez <rafael.arias.gomez@gmail.com>
2017-06-05 09:48:17 +0200

    Modificaciones 5.2

--- routes/index.js
+++ routes/index.js
@@ -25,16 +25,15 @@ router.param('quizId', quizController.load);
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/randomplay',       quizController.randomplay);
-router.get('/quizzes/randomcheck/:quizId(\\d+)',       quizController.randomcheck);
 router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
+router.get('/quizzes/randomplay',  quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
 module.exports = router;
